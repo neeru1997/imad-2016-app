5f891a1fc6bae2c4761eaa6bd040765c18816a1b
neeru1997 <neerajsarla968@gmail.com>
2016-10-15 15:14:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,27 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req, res){
+ res.send('Article one is reqeusted and will be served here')   
+    
+});
+
+ app.get('/article-two',function(req, res){
+ res.send('Article two is reqeusted and will be served here')   
+    
+});   
+
+app.get('/article-three',function(req, res){
+ res.send('Article three is reqeusted and will be served here')   
+    
+});    
+
+
+
+
+
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
