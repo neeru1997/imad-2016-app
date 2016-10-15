a584ad77e1cf6519699f5e6014a5d2dba3638725
neeru1997 <neerajsarla968@gmail.com>
2016-10-15 15:15:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
- res.send('Article one is reqeusted and will be served here')   
+ res.send('Article one is reqeusted and will be served here');   
     
 });
 
  app.get('/article-two',function(req, res){
- res.send('Article two is reqeusted and will be served here')   
+ res.send('Article two is reqeusted and will be served here') ;  
     
 });   
 
 app.get('/article-three',function(req, res){
- res.send('Article three is reqeusted and will be served here')   
+ res.send('Article three is reqeusted and will be served here');   
     
 });    
 
